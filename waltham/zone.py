ce130bd94439e1926f15282456b99f78e337373c
Timothy Riley <timothyjohnriley@gmail.com>
2023-10-29 18:01:38 -0400

    Properly use zone as dict

--- waltham/zone.py
+++ waltham/zone.py
@@ -4,18 +4,18 @@ PARKING_MINIMUM = 2.0
 
 
 class Zone(dict):
-    def __init__(self, name, front_setback, side_setback, rear_setback, height, stories, far, max_lot_coverage, min_open_space, lot_area, max_dua, lot_frontage):
-        self.name = name
-        self.front_setback = front_setback
-        self.side_setback = side_setback
-        self.rear_setback = rear_setback
-        self.height = height
-        self.stories = stories
-        self.far = far
-        self.max_lot_coverage = max_lot_coverage
-        self.min_open_space = min_open_space
-        self.lot_area = lot_area
-        self.max_dua = max_dua
-        self.lot_frontage = lot_frontage
+    def __init__(self, name, front_setback, side_setback, rear_setback, height, stories, far, max_lot_coverage, min_open_space, lot_area, max_dua, lot_frontage, parking_ratio=None):
+        self["name"] = name
+        self["front_setback"] = front_setback
+        self["side_setback"] = side_setback
+        self["rear_setback"] = rear_setback
+        self["height"] = height
+        self["stories"] = stories
+        self["far"] = far
+        self["max_lot_coverage"] = max_lot_coverage
+        self["min_open_space"] = min_open_space
+        self["lot_area"] = lot_area
+        self["max_dua"] = max_dua
+        self["lot_frontage"] = lot_frontage
 
-        self.district_parking_ratio = PARKING_MINIMUM
+        self["district_parking_ratio"] = parking_ratio if parking_ratio is not None else PARKING_MINIMUM
