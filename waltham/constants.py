2153f946969653cb8205d82b7471ae4da780c2c6
Timothy Riley <timothyjohnriley@gmail.com>
2025-09-15 20:42:25 -0400

    Migrate to uv

--- waltham/constants.py
+++ waltham/constants.py
@@ -7,4 +7,7 @@ FT_PER_STORY = 14
 MBTA_DENSITY = 15
 
 # MassGIS CRS datum
+
+# N.B. this is referenced on their webpage, but their API suggests EPSG:4326 by default, which is WGS84 (a global CRS)
+# vs. this, which is a CRS for North America instead
 MASSGIS_CRS = "NAD83"
