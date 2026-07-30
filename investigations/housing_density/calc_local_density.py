326fc8e0a61e232024d39f16634a130aa4b80549
Timothy Riley <timothyjohnriley@gmail.com>
2026-07-30 08:44:28 -0400

    Use spatial index to speed up calculation and create histogram plot of densities

--- investigations/housing_density/calc_local_density.py
+++ investigations/housing_density/calc_local_density.py
@@ -5,14 +5,21 @@ def calc_local_density(df, indexes):
     
     results = []
     
+    sindex = df.sindex
+    
     for idx in indexes:
     
         # don't calculate neighbors of non-waltham towns
         if df.at[idx, "TOWN_ID"] != 308:
             results.append((idx, 0))
             continue
+        
+        point = df.at[idx, "centroid"]
+        buffer = point.buffer(radius)
+        candidates = df.iloc[sindex.query(buffer, predicate="intersects")]
+        
 
-        neighbors = df[df["centroid"].distance(df.at[idx, "centroid"]) <= radius]
+        neighbors = candidates[candidates["centroid"].distance(point) <= radius]
         
         # only residential parcels are relevant for this calculation
         neighbors = neighbors[neighbors["USE_CODE"] < 200]
